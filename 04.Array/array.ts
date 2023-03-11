d08d2ce3a966c2806bc4b275b36b4679b750acbc
ajaysharma12799 <ajaysharma12799@gmail.com>
2023-03-12 01:20:50 +0530

    Arrays

---
+++ 04.Array/array.ts
@@ -0,0 +1,19 @@
+// using Array Literal
+let persons: string[] = ['Ajay', 'Roshan', 'Rohit', 'Sanjay'];
+console.log(persons);
+
+// using Array Class
+let marks: Array<number> = [55,65,75,85,95];
+console.log(marks);
+
+// Multi-Dimensional Array
+let matrix: number[][] = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+console.log(matrix);
+
+// Array of Mixed Type
+let anyArray: [number, string] = [1, 'Ajay']
+console.log(anyArray);
